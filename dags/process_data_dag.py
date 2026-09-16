8cec7fbf6fdbd7ad2f51137cdf5f46212cd86ecf
Taya <taya.samal@gmail.com>
2026-09-16 12:50:51 +0300

    chore: fixed syntax error

--- dags/process_data_dag.py
+++ dags/process_data_dag.py
@@ -8,47 +8,51 @@ from airflow.operators.empty import EmptyOperator
 from airflow.operators.python import BranchPythonOperator
 from airflow.sensors.filesystem import FileSensor
 
-INCOMING_FILE = '/opt/airflow/data/incoming/tiktok_google_play_reviews.csv'
+INCOMING_FILE = "/opt/airflow/data/incoming/tiktok_google_play_reviews.csv"
+
 
 def choose_branch() -> str:
     if not os.path.exists(INCOMING_FILE) or os.path.getsize(INCOMING_FILE) == 0:
-        return 'log_empty_file'
-    df = pd.read_csv(INCOMING_FILE, nrows=100)
+        return "log_empty_file"
+
+    df = pd.read_csv(INCOMING_FILE, nrows=1)
     if df.empty:
-        return 'log_empty_file'
-    return 'process_data'   
+        return "log_empty_file"
+
+    return "process_data"
+
 
 with DAG(
-    dag_id='process_tiktok_reviews',
+    dag_id="process_tiktok_reviews",
     start_date=datetime(2026, 1, 1),
-    schedule = None,
+    schedule=None,
     catchup=False,
     default_args={
-        retries: 1,
-        retry_delay: timedelta(minutes=1),
+        "retries": 1,
+        "retry_delay": timedelta(minutes=1),
     },
-    tags=['tiktok', 'reviews'],
-    doc_md="Wait for CSV, then branch: empty file vs processing."
+    tags=["tiktok", "reviews"],
+    doc_md="Wait for CSV, then branch: empty file vs processing.",
 ) as dag:
-wait_for_file = FileSensor(
-    task_id = 'wait_for_review_file',
-    filepath=INCOMING_FILE,
-    fs_conn_id='fs_default',
-    poke_interval=10,
-    timeout=60 * 60,
-    mode='poke',
-)
+    wait_for_file = FileSensor(
+        task_id="wait_for_review_file",
+        filepath=INCOMING_FILE,
+        fs_conn_id="fs_default",
+        poke_interval=10,
+        timeout=60 * 60,
+        mode="poke",
+    )
 
-branch = BranchPythonOperator(
-    task_id='branch',
-    python_callable=choose_branch,
-)
+    branch = BranchPythonOperator(
+        task_id="check_if_empty",
+        python_callable=choose_branch,
+    )
 
-log_empty_file = BashOperator(
-    task_id='log_empty_file',
-    bash_command="bash /opt/airflow/scripts/log_empty.sh "
-)
+    log_empty_file = BashOperator(
+        task_id="log_empty_file",
+        bash_command="bash /opt/airflow/scripts/log_empty.sh ",
+    )
 
-process_data = EmptyOperator(task_id='process_data')
+    process_data = EmptyOperator(task_id="process_data")
 
-wait_for_file >> branch >> [log_empty_file, process_data]
+    wait_for_file >> branch >> [log_empty_file, process_data]
